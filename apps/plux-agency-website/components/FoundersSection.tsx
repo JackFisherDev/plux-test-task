f3f39505cd4646825ab2171f6ec6b31c981991b8
JackFisherDev <yevgenrybalchenko@gmail.com>
2023-04-06 14:26:23 +0400

    feat: expand FounderCard by click with disabled Parallax effect

--- apps/plux-agency-website/components/FoundersSection.tsx
+++ apps/plux-agency-website/components/FoundersSection.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 
 import Image from 'next/image';
-import { Parallax } from 'react-scroll-parallax';
+// import { Parallax } from 'react-scroll-parallax';
 
 import { FOUNDERS } from '../common/consts';
 
@@ -12,22 +12,22 @@ import AboutUsImage from '../public/images/about-us-bg-image.png';
 const FoundersSection: FC = () => {
   return (
     <section className="px-2 py-18.5">
-      <div className="w-full max-w-[1417px] mx-auto">
+      <div className="w-full max-w-[1417px] mx-auto relative">
         <Image
           src={AboutUsImage}
           alt="About background"
           aria-hidden="true"
           className="absolute -z-10 object-cover object-center pointer-events-none"
         />
-        <Parallax translateY={[0, -75]}>
-          <ul className="pt-[158px]">
-            {FOUNDERS.map((founder) => (
-              <li key={founder.name}>
-                <FounderCard {...founder} />
-              </li>
-            ))}
-          </ul>
-        </Parallax>
+        {/* <Parallax translateY={[0, -75]}> */}
+        <ul className="pt-[158px]">
+          {FOUNDERS.map((founder) => (
+            <li key={founder.name}>
+              <FounderCard {...founder} />
+            </li>
+          ))}
+        </ul>
+        {/* </Parallax> */}
       </div>
     </section>
   );
--- apps/plux-agency-website/components/ui/FounderCard.tsx
+++ apps/plux-agency-website/components/ui/FounderCard.tsx
@@ -1,10 +1,13 @@
 import type { FC } from 'react';
 
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
+// import { motion } from 'framer-motion';
 
 import type { IFounder } from '../../types/types';
 
 import PluxIcon from '../../public/images/plux-icon.svg';
+import CrossIcon from '../../public/images/cross-icon.svg';
 
 const FounderCard: FC<IFounder> = ({
   name,
@@ -13,39 +16,132 @@ const FounderCard: FC<IFounder> = ({
   image,
   bio,
 }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openCard = () => setIsOpen(true);
+  const closeCard = () => setIsOpen(false);
+
+  const cardStateStyle = {
+    general: `bg-white/8 backdrop-blur-[6px]`,
+    closed: `max-w-[256px] relative border border-white/15 ${cardPosition}`,
+    open: `flex justify-center items-center fixed top-0 left-0 right-0 bottom-0 z-20`,
+  };
+
+  useEffect(() => {
+    const handleCloseModal = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        closeCard();
+      }
+    };
+    window.addEventListener('keydown', handleCloseModal);
+    return () => window.removeEventListener('keydown', handleCloseModal);
+  }, []);
+
   return (
-    <div
-      className={`max-w-[256px] relative bg-white/8 border border-white/15 backdrop-blur-[6px] p-4 pb-12 ${cardPosition}`}
-    >
-      <div className="relative">
-        <div>
-          <div className="relative mb-16 shadow-photo">
+    <>
+      <div
+        className={`${!isOpen ? cardStateStyle.closed : cardStateStyle.open} ${
+          cardStateStyle.general
+        }`}
+      >
+        <div className={`${!isOpen ? '' : 'w-full max-w-[1372px]'} relative`}>
+          <button
+            type="button"
+            className={`${
+              !isOpen ? 'hidden' : ''
+            } absolute top-0 right-0 z-10 cursor-pointer`}
+            onClick={closeCard}
+          >
             <Image
-              src={image}
-              alt={`${name} ${jobTitle}`}
-              width={224}
-              height={224}
+              src={CrossIcon}
+              alt="Close card button"
+              width={24}
+              height={24}
               loading="lazy"
-              className={`drop-shadow-small-photo`}
             />
-            <Image
-              src={PluxIcon}
-              alt="Plux icon"
-              width={80}
-              height={80}
-              loading="lazy"
-              className="absolute left-1/2 -bottom-[40px] z-10 -translate-x-10 pointer-events-none"
-            />
+          </button>
+          <div
+            className={`${
+              !isOpen
+                ? 'p-4 pb-16 hover:cursor-pointer'
+                : 'flex justify-start items-center gap-[55px] w-full'
+            }`}
+            onClick={openCard}
+          >
+            <div
+              className={`${
+                !isOpen ? 'mb-16' : 'flex-1 w-full max-w-[480px]'
+              } relative shadow-photo`}
+            >
+              <Image
+                src={image}
+                alt={`${name} ${jobTitle}`}
+                width={!isOpen ? 224 : 480}
+                height={!isOpen ? 224 : 480}
+                loading="lazy"
+                className={`block ${
+                  !isOpen
+                    ? 'drop-shadow-small-photo'
+                    : 'drop-shadow-large-photo w-full'
+                }`}
+              />
+            </div>
+            <div className={`${!isOpen ? '' : 'flex-1'} relative`}>
+              <h3
+                className={`${
+                  !isOpen
+                    ? 'text-center text-2xl font-bold mb-2'
+                    : 'text-[56px] font-normal mb-4 flex justify-start items-end gap-3'
+                } text-white font-gt-ultra-fine leading-none tracking-tight`}
+              >
+                {name}
+                <Image
+                  src={PluxIcon}
+                  alt="Plux icon"
+                  width={!isOpen ? 80 : 43}
+                  height={!isOpen ? 80 : 43}
+                  loading="lazy"
+                  className={`${
+                    !isOpen
+                      ? 'absolute  z-10 -translate-x-10 left-1/2 -top-[104px]'
+                      : 'static'
+                  }  pointer-events-none`}
+                />
+              </h3>
+              <p
+                className={`${
+                  !isOpen ? 'text-base text-center' : 'text-2xl'
+                }  leading-none text-yellow`}
+              >
+                {jobTitle}
+              </p>
+              <div className={`${!isOpen ? 'hidden' : ''} mt-4`}>
+                {bio.map(({ label, items }) => (
+                  <div key={`${name}-${label}`} className="mb-4">
+                    <h4 className="text-base leading-none mb-1">{label}</h4>
+                    <ul className="list-disc">
+                      {items.map((item, index) => (
+                        <li
+                          key={`${index}-${item}`}
+                          className="text-base leading-relaxed opacity-80"
+                        >
+                          {item}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <h3 className="text-white text-2xl leading-none text-center font-gt-ultra-fine font-bold tracking-wide mb-2">
-            {name}
-          </h3>
-          <p className="text-base leading-none text-yellow text-center">
-            {jobTitle}
-          </p>
         </div>
       </div>
-    </div>
+      {/*
+        NOTE: Bottom div is a placeholder that appears when the card becomes
+              "position: fixed;". It is needed to prevent other card shifting.
+      */}
+      {isOpen && <div className={`w-[256px] h-[416px] ${cardPosition}`} />}
+    </>
   );
 };
 
